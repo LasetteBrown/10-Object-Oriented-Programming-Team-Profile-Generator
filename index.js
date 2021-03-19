8587e49570ce6e097d1b9bad8aef83114afeda92
Lasette <lasette.brown@gmail.com>
2021-03-19 17:53:10 -0600

    classes and questions created.

--- index.js
+++ index.js
@@ -7,6 +7,30 @@ const Manager = require("./lib/manager");
 
 
 const allEmployees = []
+const teamCards = []
+
+function createCards() {
+
+    allEmployees.forEach(person => {
+        let card = `
+        <div class="card" style="width: 18rem;">
+
+        <div class="card-body">
+            <h5 class="card-title">${person.name}</h5>
+            <h6 class="card-text">${person.getRole()}</h6>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${person.id}</li>
+            <li class="list-group-item">email: <a href='mailto:${person.email}'>${person.email}</a>
+            </li>
+            <li class="list-group-item">${person.getOther()}:${person.other}</li>
+        </ul>
+    </div>
+`
+        teamCards.push(card)
+    });
+    console.log(teamCards)
+}
 
 inquirer
     .prompt([
@@ -16,33 +40,17 @@ inquirer
             type: 'input',
             name: 'name',
             message: "What is the team manager's name?",
-            validate: async (input) => {
-                if (typeof input !== "string") {
-                    return 'Invalid entry';
-                }
-            },
         },
         {
             type: 'input',
             name: 'id',
             message: "What is the team manager's employee ID?",
-            validate: async (input) => {
-                if (typeof input !== "number") {
-                    return 'Invalid entry';
-                }
-            },
 
         },
         {
             type: 'input',
             name: 'email',
             message: "What is the team manager's email address?",
-            validate: async (input) => {
-                const required = '@'
-                if (!required.test(input)) {
-                    return 'Invalid entry';
-                }
-            },
 
 
         },
@@ -50,45 +58,155 @@ inquirer
             type: 'input',
             name: 'managerOffice',
             message: "What is the team manager's office number?",
-            validate: async (input) => {
-                if (typeof input !== "number") {
-                    return 'Invalid entry';
-                }
-            },
 
         },
 
         // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-        {
-            type: 'list',
-            message: 'Would you like to add an additional employee?',
-            name: 'moreEmplyees',
-            choices: ['add an engineer', 'add an inturn', 'finished adding'],
-            default: 'finished adding',
-        },
 
     ])
     .then((data) => {
         const manager = new Manager(data.name, data.id, data.email, data.managerOffice)
         allEmployees.push(manager)
-
-        if (data.moreEmplyees === 'add an engineer') {
-            //run inquirer again with additional questions
-        } else if (data.moreEmplyees === 'add an inturn') {
-            //run inquirer again with additional questions
-        } else {
-            const htmlTemplate = ``
-            //then the file is written into the directory. Any existing readme file will be overwritten.
-            fs.writeFile('../dist/index.html', htmlTemplate, (err) =>
-                err ? console.log(err) : console.log('Success!')
-            );
-
-        }
+        askMore()
 
     }
     )
 
+function askMore() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Would you like to add an additional employee?',
+                name: 'moreEmplyees',
+                choices: ['add an engineer', 'add an inturn', 'finished adding'],
+                default: 'finished adding',
+            },
 
+        ])
+        .then((data) => {
+            if (data.moreEmplyees === 'add an engineer') {
+                createEngineer()
+            } else if (data.moreEmplyees === 'add an inturn') {
+                createIntern()
+            } else {
+                console.log(allEmployees)
+                createCards()
+                const htmlTemplate = `<!doctype html>
+                <html lang="en">
+                
+                <head>
+                    <!-- Required meta tags -->
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                
+                    <!-- Bootstrap CSS -->
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+                        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+                    <link href="./style.css" rel="stylesheet">
+                    <title>My Team</title>
+                </head>
+                
+                <body>
+                    <header>
+                        <h1>My Team</h1>
+                
+                    </header>
+                    <main>
+                    ${teamCards.join('')}
+                    </main>
+                
+                
+                    <!-- Option 1: Bootstrap Bundle with Popper -->
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+                        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+                        crossorigin="anonymous"></script>
+                
+                </body>
+                
+                </html>`
+                //then the file is written into the directory. Any existing readme file will be overwritten.
+                fs.writeFile('../dist/index.html', htmlTemplate, (err) =>
+                    err ? console.log(err) : console.log('Success!')
+                );
+
+            }
+
+        })
+
+}
+
+
+function createEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the engineer's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the engineer's employee ID?",
+
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the engineer's email address?",
+            },
+            {
+                type: 'input',
+                name: 'gitUser',
+                message: "What is the engineer's Github Username?",
+            },
+        ])
+        .then((data) => {
+            const engineer = new Engineer(data.name, data.id, data.email, data.gitUser)
+            allEmployees.push(engineer)
+            askMore()
+
+        }
+        )
+
+
+}
+
+function createIntern() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the intern's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the intern's employee ID?",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the intern's email address?",
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "What is the intern's school?",
+            },
+        ])
+        .then((data) => {
+            const intern = new Intern(data.name, data.id, data.email, data.school)
+            allEmployees.push(intern)
+            askMore()
+
+        }
+        )
+
+
+}
 //pseudocode
 // GIVEN a command-line application that accepts user input
 // WHEN I am prompted for my team members and their information
