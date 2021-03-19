66e000aef6507d4433497331bb35856dd8a3880c
Lasette <lasette.brown@gmail.com>
2021-03-19 13:08:45 -0600

    created package.json and dependencies.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-//pseudocode
 
+
+//pseudocode
+//create a package.json which includes jest and inquirer as dependencies
 // GIVEN a command-line application that accepts user input
 // WHEN I am prompted for my team members and their information
 // THEN an HTML file is generated that displays a nicely formatted team roster based on user input
